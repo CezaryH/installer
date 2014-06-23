aaf9784ee093d4681e9257da2575b15de3650ed4
Cezary Haller <cezary.haller@gmail.com>
2014-06-23 16:58:01 +0200

    variable for current user directory

--- clearComposerCache.bat
+++ clearComposerCache.bat
@@ -1,3 +1,4 @@
-FOR /D %%p IN ("C:\Users\lap\AppData\Local\Composer\files\*.*") DO rmdir "%%p" /s /q
-FOR /D %%p IN ("C:\Users\lap\AppData\Local\Composer\repo\*.*") DO rmdir "%%p" /s /q
-FOR /D %%p IN ("C:\Users\lap\AppData\Local\Composer\vcs\*.*") DO rmdir "%%p" /s /q
+@echo off
+FOR /D %%p IN ("%UserProfile%\AppData\Local\Composer\files\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("%UserProfile%\AppData\Local\Composer\repo\*.*") DO rmdir "%%p" /s /q
+FOR /D %%p IN ("%UserProfile%\AppData\Local\Composer\vcs\*.*") DO rmdir "%%p" /s /q
